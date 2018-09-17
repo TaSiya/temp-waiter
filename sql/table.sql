8ffeb91ef2b2280c22e750a9ada5d934c621d328
tasiya <siyandamakhalima.1@gmail.com>
2018-09-17 12:52:40 +0200

    changed the status column

--- sql/table.sql
+++ sql/table.sql
@@ -8,7 +8,8 @@ create table waiters(
 
 create table weekdays(
     id serial primary key not null,
-    day text not null
+    day text not null,
+    status text
 
 );
 
@@ -16,7 +17,6 @@ create table shifts (
     id serial primary key not null,
     waiter_id int not null,
     weekday_id int not null,
-    status text,
     FOREIGN key (waiter_id) REFERENCES waiters(id),
     FOREIGN KEY (weekday_id) REFERENCES weekdays(id)
 );
