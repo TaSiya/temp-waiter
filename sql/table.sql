c2a4f1d3a4409eb4631bf45dac89dc44255d7389
tasiya <siyandamakhalima.1@gmail.com>
2018-09-18 17:03:53 +0200

    made the template more useful

--- sql/table.sql
+++ sql/table.sql
@@ -1,22 +1,24 @@
+drop table waiters, weekdays, shifts;
 
 create table waiters(
     id serial primary key not null,
     first_name text not null,
     passcode int not null
 );
-
-
+alter sequence waiters_id_seq restart 1;
 create table weekdays(
     id serial primary key not null,
     day text not null,
-    status text
+    status text,
+    box text
 );
-
+alter SEQUENCE weekdays_id_seq restart 1;
 create table shifts (
     id serial primary key not null,
     waiter_id int not null,
     weekday_id int not null,
-    box text,
     FOREIGN key (waiter_id) REFERENCES waiters(id),
     FOREIGN KEY (weekday_id) REFERENCES weekdays(id)
 );
+
+\i sql/insertEmployees.sql;
